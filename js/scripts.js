353c541886df94d588b280197a173ee72823739f
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-08-11 09:49:14 -0700

    Add radio buttons, begin writing js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $('.c').click(function () {
+   document.getElementsByClassName("c").innerHTML = cUp();
+  });
+
+  $('.ruby').click(function () {
+   document.getElementsByClassName("ruby").innerHTML = rubyUp();
+  });
+
+  $('.java').click(function () {
+   document.getElementsByClassName("java").innerHTML = javaUp();
+  });
+
+
+
+
+});
