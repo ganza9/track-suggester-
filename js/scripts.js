189f4fbd31e2d35b8cacf15ec3ffcba1a57668bd
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-08-11 11:17:53 -0700

    fix result bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-
 var c = 0;
 var ruby = 0;
 var java = 0;
-var modal = document.getElementById('myModal');
+var button = document.getElementById('button');
+
 
 function cUp()
 {
@@ -19,25 +19,34 @@ function javaUp()
   java = java + 1;
 }
 
+function getResult() {
+  if(c > ruby && c > java)
+  {
+    alert("You should learn C#");
+  }
+  else if(ruby > c && ruby > java)
+  {
+    alert("You should learn Ruby");
+  }
+  else if(java > c && java > ruby)
+  {
+    alert("You should learn Java");
+  }
+}
 
 
 
+$(document).ready(function()
+{
+    $('.c').click(function(){
+     document.getElementsByClassName("c").innerHTML = cUp();
+    });
 
-$(document).ready(function(){
+    $('.ruby').click(function(){
+     document.getElementsByClassName("ruby").innerHTML = rubyUp();
+    });
 
-  $('.c').click(function () {
-   document.getElementsByClassName("c").innerHTML = cUp();
-  });
-
-  $('.ruby').click(function () {
-   document.getElementsByClassName("ruby").innerHTML = rubyUp();
-  });
-
-  $('.java').click(function () {
-   document.getElementsByClassName("java").innerHTML = javaUp();
-  });
-
-
-
-
-});
+    $('.java').click(function(){
+     document.getElementsByClassName("java").innerHTML = javaUp();
+   });
+ });
