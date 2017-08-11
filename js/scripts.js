893ba3514f06dfafa5d13dbc81988fe8c82fbde9
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-08-11 10:18:40 -0700

    Finalize questions in forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+
+var c = 0;
+var ruby = 0;
+var java = 0;
+var modal = document.getElementById('myModal');
+
+function cUp()
+{
+  c = c + 1;
+}
+
+function rubyUp()
+{
+  ruby = ruby + 1;
+}
+
+function javaUp()
+{
+  java = java + 1;
+}
+
+
+
+
+
 $(document).ready(function(){
 
   $('.c').click(function () {
